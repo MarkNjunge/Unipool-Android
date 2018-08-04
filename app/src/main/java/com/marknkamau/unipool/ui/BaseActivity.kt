751b278421910d3e9fff2648d2500ac94e5aaefe
Mark Njung'e <mark.kamau@outlook.com>
2018-08-04 18:37:57 +0300

    Fix sign out

--- app/src/main/java/com/marknkamau/unipool/ui/BaseActivity.kt
+++ app/src/main/java/com/marknkamau/unipool/ui/BaseActivity.kt
@@ -50,8 +50,8 @@ open class BaseActivity : AppCompatActivity(), AuthenticationService.SignOutList
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_sign_out -> {
-                authService.signOut(this)
-                localStorage.deleteUser()
+                UnipoolApp.authService.signOut(this)
+                UnipoolApp.localStorage.deleteUser()
                 true
             }
             R.id.menu_profile -> {
